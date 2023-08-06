0eb509ce771e55ca4e12ef507cd120c7a6fef767
Facu002 <achafacundoagustin@gmail.com>
2023-08-06 18:56:52 -0300

    added mobile nav menu

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -37,12 +37,9 @@ export function NavBar() {
 
     useEffect(() => {
         if (mobileNavMenu == true) {
-            console.log('isOpen');
             setToggleNavMenu('open')
         }else{
-            console.log('isClosed');
             setToggleNavMenu('closed')
-
         }
     }, [mobileNavMenu])
     
@@ -62,13 +59,13 @@ export function NavBar() {
                 <div className={styles.Navbar_container__hamburger} onClick={()=>setMobileNavMenu(!mobileNavMenu)}>
                     {
                         mobileNavMenu ? 
-                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-x" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#502314" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-x" width="40" height="40" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#502314" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                             <path d="M18 6l-12 12" />
                             <path d="M6 6l12 12" />
                         </svg>
                         :
-                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-menu-2" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#502314" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-menu-2" width="40" height="40" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#502314" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                             <path d="M4 6l16 0" />
                             <path d="M4 12l16 0" />
