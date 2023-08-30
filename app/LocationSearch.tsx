a7ff2dd9d77a64a1b69a78fe27bea6d1506d1177
Facu002 <achafacundoagustin@gmail.com>
2023-08-30 16:08:12 -0300

    location seach added

--- app/LocationSearch.tsx
+++ app/LocationSearch.tsx
@@ -1,7 +1,10 @@
+'use client'
 import Image from "next/image"
+import { useState } from "react"
 import styles from './page.module.css'
 import LocationImage from './assets/images/BK_ILLUSTRATION_PICKLE_EYE_MASK_ST_FC.png'
 export default function LocationSearchContainer(){
+    const [searchValue, setSearchValue] = useState<string>('');
 
     return(
         <article className={styles.LocalSearch_container}>
@@ -10,14 +13,15 @@ export default function LocationSearchContainer(){
                 <span>Encontrá el mas cercano</span>
 
                 <div className={styles.inputContainer}>
-                    <input type="text" name="search" id="search" placeholder="Tu ciudad o direccion"/>
-                    <button type="button" name="searchButton" id="search">
+                    <input type="text" name="search" id="search" placeholder="Tu ciudad o direccion" 
+                    value={searchValue} onChange={(e) => setSearchValue(e.target.value)}/>
+                    <a  target="_blank" href={`https://www.google.com.ar/maps/search/${encodeURIComponent(searchValue)},+burger+king`}>
                         <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-send" width="30" height="30" viewBox="0 0 24 24" strokeWidth="2.5" stroke="#502314" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                             <path d="M10 14l11 -11" />
                             <path d="M21 3l-6.5 18a.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a.55 .55 0 0 1 0 -1l18 -6.5" />
                         </svg>
-                    </button>
+                    </a>
                 </div>
             </div>
             <div className={styles.LocalSearch_container__image}>
--- app/locations/page.tsx
+++ app/locations/page.tsx
@@ -67,7 +67,16 @@ export default function page(params:any) {
                                     <div key={local.name} className={styles.localInformation}>
                                         <span>{local.name}</span>
                                         <p>{local.dir}</p>
-                                        <a href={local.maps}>Abir Direccion</a>
+                                        <a target="_blank" href={local.maps}>
+                                            Abir Direccion 
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-map-pin-share" width="15" height="15" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#E54929" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                            <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+                                            <path d="M12.02 21.485a1.996 1.996 0 0 1 -1.433 -.585l-4.244 -4.243a8 8 0 1 1 13.403 -3.651" />
+                                            <path d="M16 22l5 -5" />
+                                            <path d="M21 21.5v-4.5h-4.5" />
+                                            </svg>
+                                        </a>
                                     </div>))
                                     }
                                     </div>
